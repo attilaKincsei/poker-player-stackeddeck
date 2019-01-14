c74be0dcdc2e9ab58abf04f3f42bf8e1199bf301
SinyiMark <35994165+SinyiMark@users.noreply.github.com>
2019-01-14 10:51:34 +0100

    Log min raise

--- player.py
+++ player.py
@@ -1,8 +1,13 @@
+import json
+import sys
+from collections import namedtuple
 
 class Player:
     VERSION = "0.1"
 
     def betRequest(self, game_state):
+        x = json.loads(game_state, object_hook=lambda d: namedtuple('X', d.keys())(*d.values()))
+        sys.stderr.write(x["minimum_raise"])
         return 240
 
     def showdown(self, game_state):
